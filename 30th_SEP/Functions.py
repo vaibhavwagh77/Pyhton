60b33584cd3c1545440377fb5a956205c26141c5
vaibhavwagh77 <vaibhavw2001@gmail.com>
2024-09-30 22:49:40 +0530

    firstcommit

---
+++ 30th_SEP/Functions.py
@@ -0,0 +1,10 @@
+def addNumbers(num1,num2):
+    result=num1+num2
+    return result  
+
+
+num1=int(input("Enter the number: "))
+num2=int(input("Enter the number: "))
+res=addNumbers(num1,num2)
+print(res)
+
